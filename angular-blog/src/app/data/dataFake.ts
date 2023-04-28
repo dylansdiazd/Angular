9a9d3f46475a8f8dcbcab954e6835be60fb35d57
diaz <dylansantosdiaz@hotmail.com>
2023-04-27 22:37:41 -0300

    Finalizando o blog

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,13 +1,27 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id": "1",
+        "title": "Resident Evil 4 Remake finalmente é lançado!",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu tortor, faucibus vitae posuere sed, lacinia id libero. Donec tempor enim sem, ac auctor elit facilisis a. Duis tortor magna, porttitor vitae venenatis in, pulvinar auctor ante. Fusce sit amet tempus nisl, sit amet lobortis lorem. Pellentesque fermentum eu magna sed facilisis. Aliquam porttitor ornare accumsan. Morbi ac malesuada ligula. Integer mollis egestas metus ac euismod. Phasellus elementum, eros et maximus bibendum, libero sapien placerat elit, porta euismod leo diam nec ante.",
+        "photoCover": "https://image.api.playstation.com/vulcan/ap/rnd/202210/0706/EVWyZD63pahuh95eKloFaJuC.png"
     },
     {
-        "id": 2,
-        "photo": "aaa",
-        "title": "saiu novo filme",
-        "description": "ola"
-    }]
+        "id": "2",
+        "title": "Diablo IV lançara no dia 06 de Junho de 2023",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu tortor, faucibus vitae posuere sed, lacinia id libero. Donec tempor enim sem, ac auctor elit facilisis a. Duis tortor magna, porttitor vitae venenatis in, pulvinar auctor ante. Fusce sit amet tempus nisl, sit amet lobortis lorem. Pellentesque fermentum eu magna sed facilisis. Aliquam porttitor ornare accumsan. Morbi ac malesuada ligula. Integer mollis egestas metus ac euismod. Phasellus elementum, eros et maximus bibendum, libero sapien placerat elit, porta euismod leo diam nec ante.",
+        "photoCover": "https://i.ytimg.com/vi/tflKwXOESyc/maxresdefault.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Hogwarts Legacy é um sucesso em vendas",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu tortor, faucibus vitae posuere sed, lacinia id libero. Donec tempor enim sem, ac auctor elit facilisis a. Duis tortor magna, porttitor vitae venenatis in, pulvinar auctor ante. Fusce sit amet tempus nisl, sit amet lobortis lorem. Pellentesque fermentum eu magna sed facilisis. Aliquam porttitor ornare accumsan. Morbi ac malesuada ligula. Integer mollis egestas metus ac euismod. Phasellus elementum, eros et maximus bibendum, libero sapien placerat elit, porta euismod leo diam nec ante.",
+        "photoCover": "https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacy_AvalancheSoftware_S1_2560x1440-2baf3188eb3c1aa248bcc1af6a927b7e"
+    },
+    {
+        "id": "4",
+        "title": "Tekken 8 ja esta em Desenvolvimento",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu tortor, faucibus vitae posuere sed, lacinia id libero. Donec tempor enim sem, ac auctor elit facilisis a. Duis tortor magna, porttitor vitae venenatis in, pulvinar auctor ante. Fusce sit amet tempus nisl, sit amet lobortis lorem. Pellentesque fermentum eu magna sed facilisis. Aliquam porttitor ornare accumsan. Morbi ac malesuada ligula. Integer mollis egestas metus ac euismod. Phasellus elementum, eros et maximus bibendum, libero sapien placerat elit, porta euismod leo diam nec ante.",
+        "photoCover": "https://image.api.playstation.com/vulcan/ap/rnd/202212/1621/rCvLik1PKDSoCaEvTIx42H3Q.png"
+    },
+
+]
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -9,19 +10,32 @@ import { ActivatedRoute } from '@angular/router';
 export class ContentComponent implements OnInit {
 
   @Input()
-  photoCover: string = "https://image.api.playstation.com/vulcan/ap/rnd/202210/0706/EVWyZD63pahuh95eKloFaJuC.png"
+  photoCover: string = ""
   @Input()
-  cardTitle: string = "Resident Evil 4 Remake Finalmente é lançado!"
+  contentTitle: string = ""
   @Input()
-  cardDescription: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu tortor, faucibus vitae posuere sed, lacinia id libero. Donec tempor enim sem, ac auctor elit facilisis a. Duis tortor magna, porttitor vitae venenatis in, pulvinar auctor ante. Fusce sit amet tempus nisl, sit amet lobortis lorem. Pellentesque fermentum eu magna sed facilisis. Aliquam porttitor ornare accumsan. Morbi ac malesuada ligula. Integer mollis egestas metus ac euismod. Phasellus elementum, eros et maximus bibendum, libero sapien placerat elit, porta euismod leo diam nec ante."
+  contentDescription: string = ""
+  private id: string | null = "0"
 
-  constructor( private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-this.route.paramMap.subscribe( value =>
-  console.log(value.get("id")))
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id"))
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 }
+
+
 
 
 
